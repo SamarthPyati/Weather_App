0a3a244b5661c3a58a3e651869b71c1a8341a7e6
SamarthPyati <samarthpyati@gmail.com>
2023-09-21 18:06:46 +0530

    Updated more context

--- main.py
+++ main.py
@@ -3,22 +3,36 @@ import json
 
 city = "Bangalore"
 
-BASE_URL = "http://api.weatherapi.com/v1/forecast.json"
+BASE_URL = "http://api.weatherapi.com/v1"
 
-data = {
+weather_data = {
     'key': "33c6bd00e41b40a7af052341232109",
-    'q': city
+    'q': city,
 }
 
-response = requests.get(BASE_URL, params=data)
+air_data = {
+    'key': "33c6bd00e41b40a7af052341232109",
+    'q': city,
+    'aqi':'yes'
+}
 
-print(response.status_code)
+weather_response = requests.get(f"{BASE_URL}/forecast.api", params=weather_data)
+air_response = requests.get(f"{BASE_URL}/current/airquality", params=air_data)
+# print(response.status_code)
 
 try:
-    if response.status_code == 200:
-        weather_data = response.json()
-        with open('data_json.json', 'w') as file:
+    if weather_response.status_code == 200:
+        weather_data = weather_response.json()
+        with open('weather_data.json', 'w') as file:
             json.dump(weather_data, file, indent = 2)
 
 except Exception as e:
-    print(f"ERROR: {e}")
+    print(f"ERROR (WEATHER): {e}")
+
+try:
+    if air_response.status_code == 200:
+        air_data = air_response.json()
+        with open('air_data.json', 'w') as file:
+            json.dump(air_data, file, indent = 2)
+except Exception as e:
+    print(f"ERROR (AIR_DATA): {e}")
